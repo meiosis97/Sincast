ace8b5ac015a938c1f950b02301310d5f3dbdd3e
Yidi <51260324+meiosis97@users.noreply.github.com>
2021-11-08 22:10:04 +0800

    Update matMult.cpp

--- matMult.cpp
+++ matMult.cpp
@@ -1,4 +1,5 @@
- // [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
+// https://stackoverflow.com/questions/35923787/fast-large-matrix-multiplication-in-r
+// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
 
  #include <RcppArmadillo.h>
  #include <RcppEigen.h>
